a5a29923d2d403208a34f27257214265542b7101
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-05-31 10:43:04 -0400

    Add search option by category to refresh API

--- src/App.js
+++ src/App.js
@@ -2,31 +2,47 @@ import React, { useEffect,  useState } from 'react'
 import './App.css';
 import NewsList from './components/newsList';
 import Axios from 'axios'
-//import Pagination from './components/pagination';
 import ReactPaginate from 'react-paginate'
 
 function App() {
 
-  const[articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const [articlesPerPage] = useState(5);
-  const pagesVisited = currentPage * articlesPerPage;
+
+
+
+const [selects, setSelects] = useState("general");
+// const path="https://newsapi.org/v2/top-headlines?country=ca&category="
+// const api = "&apiKey=3646d87ef0104aecb3697a03108d06af"
+//setSelects("general")
+//let url=path+selects+api;
   useEffect(()=> {
       const getArticles = async()=>{
         setLoading(true);
           const res= await Axios.get(
-              "https://newsapi.org/v2/top-headlines?country=us&apiKey=3646d87ef0104aecb3697a03108d06af");
-          
+              "https://newsapi.org/v2/top-headlines?country=ca&apiKey=3646d87ef0104aecb3697a03108d06af");
           setArticles(res.data.articles);
           console.log(res);
           setLoading(false);
       };
       getArticles();
   }, []);
+
+
+  const refetch = ()=>{
+    setLoading(true);
+    Axios.get(
+        `https://newsapi.org/v2/top-headlines?country=ca&category=${selects}&apiKey=3646d87ef0104aecb3697a03108d06af`
+        ).then(response=>setArticles(response.data.articles));
+        setLoading(false);
+  }
+
   // const indexOfLastPost = currentPage * articlesPerPage;
   // const indexOfFirstPost = indexOfLastPost - articlesPerPage;
   // const currentArticles = articles.slice(indexOfFirstPost, indexOfLastPost);
+  const pagesVisited = currentPage * articlesPerPage;
   const currentArticles = articles.slice(pagesVisited, pagesVisited+articlesPerPage);
 
   //const paginate = pageNumber => setCurrentPage(pageNumber);
@@ -38,6 +54,18 @@ function App() {
 
   return (
     <div className="App">
+        <div id = "topic-wrapper">
+        <label for="topics">By Topic </label>
+        <h3>{selects}</h3>
+        <select value={selects} onChange={e=>setSelects(e.target.value)}>
+          <option value="general">--------</option>
+          <option value="general">General</option>
+          <option value="business">Business</option>
+          <option value="health">Health</option>
+          <option value="technology">Technology</option>
+        </select>
+        <input id="search" name="submit" type="submit" value="Search" onClick={refetch}/>
+        </div> 
       <NewsList articles={currentArticles} loading={loading}/>
       <ReactPaginate
         previousLabel={"<"}
@@ -52,11 +80,6 @@ function App() {
         disabledClassName={"paginationDisabled"}
         activeClassName={"paginationActive"}
         />
-      {/* <Pagination
-        articlesPerPage={articlesPerPage}
-        totalArticles={articles.length}
-        paginate={paginate}
-      /> */}
     </div>
   );
 }
